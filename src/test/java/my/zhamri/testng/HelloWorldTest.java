4ae6862412f39018b13777845047e844eb6e506c
zhamri <zhamri@gmail.com>
2025-11-27 21:00:24 +0800

    Initial commit

---
+++ src/test/java/my/zhamri/testng/HelloWorldTest.java
@@ -0,0 +1,21 @@
+package my.zhamri.testng;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void test1() {
+        System.out.println("Test-1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Test-2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("Test-3");
+    }
+}
